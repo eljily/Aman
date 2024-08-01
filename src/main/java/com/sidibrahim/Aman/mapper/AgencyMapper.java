527677c171b068d3f15f96eabbc5f2f2dcf3b42c
eljily <eljilysidibrahim@gmail.com>
2024-08-01 18:05:10 +0000

    Added agencyMapper logic

--- src/main/java/com/sidibrahim/Aman/mapper/AgencyMapper.java
+++ src/main/java/com/sidibrahim/Aman/mapper/AgencyMapper.java
@@ -5,6 +5,8 @@ import com.sidibrahim.Aman.entity.Agency;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 @RequiredArgsConstructor
 public class AgencyMapper {
@@ -15,6 +17,22 @@ public class AgencyMapper {
                 .agencyCode(agencyDto.getAgencyCode())
                 .address(agencyDto.getAddress())
                 .build();
+    }
+
+    public AgencyDto toAgencyDto(Agency agency) {
+        return AgencyDto.builder()
+                .name(agency.getName())
+                .agencyCode(agency.getAgencyCode())
+                .address(agency.getAddress())
+                .build();
+    }
+
+    public List<AgencyDto> toAgencyDtos(List<Agency> agencyList) {
+        return agencyList.stream().map(this::toAgencyDto).toList();
+    }
+
+    public List<Agency> toAgencyList(List<AgencyDto> agencyDtoList) {
+        return agencyDtoList.stream().map(this::toAgency).toList();
     }
 
 }
