33f059ef7ca01d6cdfa86da861826fe52e356ba6
eljily <eljilysidibrahim@gmail.com>
2024-08-08 11:29:37 +0000

    Added Response Message to UserController

--- src/main/java/com/sidibrahim/Aman/controller/UserController.java
+++ src/main/java/com/sidibrahim/Aman/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.sidibrahim.Aman.controller;
 
+import com.sidibrahim.Aman.dto.ResponseMessage;
 import com.sidibrahim.Aman.dto.UserDto;
 import com.sidibrahim.Aman.service.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -21,13 +23,23 @@ public class UserController {
 
     @GetMapping()
     @PreAuthorize("hasAuthority('SUPER_ADMIN')")
-    public ResponseEntity<List<UserDto>> getAllUsers() {
-        return ResponseEntity.ok(userService.getAllUsers());
+    public ResponseEntity<ResponseMessage> getAllUsers() {
+        return ResponseEntity.ok(ResponseMessage
+                .builder()
+                .message("Users List Retrieved Successfully")
+                .status(HttpStatus.OK.value())
+                .data(userService.getAllUsers())
+                .build());
     }
 
     @PostMapping
     @PreAuthorize("!hasAuthority('AGENT')")
-    public ResponseEntity<UserDto> save(@RequestBody UserDto userDto) {
-        return ResponseEntity.ok(userService.addUser(userDto));
+    public ResponseEntity<ResponseMessage> save(@RequestBody UserDto userDto) {
+        return ResponseEntity.ok(ResponseMessage
+                .builder()
+                .message("User Saved Successfully")
+                .status(HttpStatus.OK.value())
+                .data(userService.addUser(userDto))
+                .build());
     }
 }
